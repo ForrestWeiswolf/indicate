03a59dce34ac1c26d999ba11aa747cb7810ee6ed
Forrest Wolf <forrest.weiswolf@gmail.com>
2023-05-10 15:29:36 -0400

    Refactor model tests

--- posts/test_models.py
+++ posts/test_models.py
@@ -3,70 +3,57 @@ from django.utils import timezone
 
 from .models import Post, Tag, Tagging
 
-class ModelTests(TestCase):
+class PostTests(TestCase):
+  def setUp(self):
+    self.post = Post(
+      title="The King in Yellow",
+      content="https://www.gutenberg.org/files/8492/8492-h/8492-h.htm",
+      creation_date=timezone.now(),
+      type=Post.PostTypes.LINK
+    )
+    self.post.save()
+
+    self.tag = Tag(
+      name="Fiction",
+      creation_date=timezone.now(),
+    )
+    self.tag.save()
 
   def test_post_can_be_created_with_properties(self):
-    post = Post(
-      title="The King in Yellow",
-      content="https://www.gutenberg.org/files/8492/8492-h/8492-h.htm",
-      creation_date=timezone.now(),
-      type=Post.PostTypes.LINK
-    )
-
-    self.assertIs(post.title, "The King in Yellow")
-    self.assertIs(post.content, "https://www.gutenberg.org/files/8492/8492-h/8492-h.htm")
-    self.assertIs(post.creation_date.day, timezone.now().day)
-    self.assertIs(post.type, Post.PostTypes.LINK)
+    self.assertIs(self.post.title, "The King in Yellow")
+    self.assertIs(self.post.content, "https://www.gutenberg.org/files/8492/8492-h/8492-h.htm")
+    self.assertIs(self.post.creation_date.day, timezone.now().day)
+    self.assertIs(self.post.type, Post.PostTypes.LINK)
 
   def test_post_has_tags(self):
-    tag = Tag(
-      name="Fiction",
-      creation_date=timezone.now(),
-    )
-    tag.save()
+    self.post.tags.add(self.tag, through_defaults={"manual": False})
 
-    post = Post(
-      title="The King in Yellow",
-      content="https://www.gutenberg.org/files/8492/8492-h/8492-h.htm",
-      creation_date=timezone.now(),
-      type=Post.PostTypes.LINK
-    )
-    post.save()
-
-    post.tags.add(tag, through_defaults={"manual": False})
-
-    self.assertEqual(post.tags.get(id=1).name, "Fiction")
-
+    self.assertEqual(self.post.tags.get(id=1).name, "Fiction")
 
 class TagTests(TestCase):
-
-  def test_can_be_created_with_properties(self):
-    tag = Tag(
-      name="Fiction",
-      creation_date=timezone.now(),
-    )
-
-    self.assertIs(tag.name, "Fiction")
-    self.assertIs(tag.creation_date.day, timezone.now().day)
-
-  def test_tag_has_post(self):
-    post = Post(
+  def setUp(self):
+    self.post = Post(
       title="The King in Yellow",
       content="https://www.gutenberg.org/files/8492/8492-h/8492-h.htm",
       creation_date=timezone.now(),
       type=Post.PostTypes.LINK
     )
-    post.save()
+    self.post.save()
 
-    tag = Tag(
+    self.tag = Tag(
       name="Fiction",
       creation_date=timezone.now(),
     )
-    tag.save()
+    self.tag.save()
 
-    post.tags.add(tag, through_defaults={"manual": False})
+  def test_can_be_created_with_properties(self):
+    self.assertIs(self.tag.name, "Fiction")
+    self.assertIs(self.tag.creation_date.day, timezone.now().day)
 
-    self.assertEqual(tag.post_set.get(id=1).title, "The King in Yellow")
+  def test_tag_has_post(self):
+    self.post.tags.add(self.tag, through_defaults={"manual": False})
+
+    self.assertEqual(self.tag.post_set.get(id=1).title, "The King in Yellow")
 
 class TaggingTests(TestCase):
 
