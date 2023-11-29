8d7832f7e4bab1f2bd3af90caf2a54f727ea256a
Forrest Wolf <forrest.weiswolf@gmail.com>
2023-11-29 10:25:09 -0500

    Add action for viewing posts by tag

--- posts/test_views.py
+++ posts/test_views.py
@@ -50,14 +50,33 @@ class PostListViewTest(TestCase):
 class TagListViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        tags = [Tag.objects.create(name="Fiction")]
+        posts = [
+            Post.objects.create(
+                title="When William Came",
+                content="https://www.gutenberg.org/cache/epub/14540/pg14540-images.html",
+                type=Post.PostTypes.LINK,
+            ),
+            Post.objects.create(
+                title="Django Rest Framework",
+                content="https://www.django-rest-framework.org/",
+                type=Post.PostTypes.LINK,
+            ),
+        ]
+        posts[0].tags.add(Tag.objects.create(name="Fiction"), through_defaults={"manual": True})
+        posts[1].tags.add(Tag.objects.create(name="Software"), through_defaults={"manual": True})
 
     def test_view_tags(self):
         response = self.client.get("/posts/tags/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data[0]["name"], "Fiction")
+        self.assertEqual(response.data[0]["name"], "Software")
+        self.assertEqual(response.data[1]["name"], "Fiction")
 
     def test_view_single_tag(self):
         response = self.client.get("/posts/tags/1/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["name"], "Fiction")
+
+    def test_view_posts_by_tag(self):
+        response = self.client.get("/posts/tags/1/posts/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data[0]["title"], "When William Came")
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from .models import Post, Tag
 from .serializers import PostSerializer, TagSerializer
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-creation_date")
@@ -11,3 +12,10 @@ class PostViewSet(viewsets.ModelViewSet):
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all().order_by("-creation_date")
     serializer_class = TagSerializer
+
+    @action(detail=True)
+    def posts(self, request, *args, **kwargs):
+        tag = self.get_object()
+        posts = tag.post_set.all()
+        posts_json = PostSerializer(posts, many=True, context={'request': request}).data
+        return Response(posts_json)
